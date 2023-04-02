703026b245b068bf352e0417fb367f1e8208540a
nothingnothings <nothingnothings@users.noreply.github.com>
2023-04-02 14:31:25 +0000

    deploy: aca4240a55e95af7e10fd43c1e2f1985d5d6d928

--- shared.js
+++ shared.js
@@ -2,7 +2,6 @@ var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector('.modal__action--negative');
 var selectPlanButtons = document.querySelectorAll('.plan button');
-var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 var ctaButton = document.querySelector('.main-nav__item--cta');
 var selectPlanButtons = document.querySelectorAll('.plan button');
